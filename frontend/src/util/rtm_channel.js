70cf0134ce08afb4ffd6684fe6f0e6925618bb32
Rashi Abramson <roshkins@gmail.com>
2017-09-09 16:26:04 -0700

    got it to publish

--- frontend/src/util/rtm_channel.js
+++ frontend/src/util/rtm_channel.js
@@ -1,10 +1,10 @@
 import * as RTM from "satori-rtm-sdk";
 class RTMChannel {
   constructor(channelName, dataCallback, errorCallback) {
+    this.channelName = channelName;
     const appkey = "ACefAD440eE8EdeF52cf9eeACA0d5A1e";
     const endpoint = "wss://ywf1mnw0.api.satori.com";
     this.client = new RTM(endpoint, appkey);
-    console.log(this.client);
     this.client.on("error", function(error) {
       var reason;
       if (error.body) {
@@ -16,17 +16,25 @@ class RTMChannel {
       errorCallback(error.body || "unknown error");
     });
 
-    const channel = this.client.subscribe(
+    this.channel = this.client.subscribe(
       channelName,
       RTM.SubscriptionMode.SIMPLE
     );
 
     /* set callback for PDU with specific action */
-    channel.on("rtm/subscription/data", function(pdu) {
+    this.channel.on("rtm/subscription/data", function(pdu) {
       pdu.body.messages.forEach(msg => dataCallback(msg));
     });
 
     this.client.start();
+  }
+
+  publish(data) {
+    if (this.client.isConnected()) {
+      this.client.publish(this.channelName, data);
+    } else {
+      setTimeout(() => this.publish(data), 100);
+    }
   }
 }
 
