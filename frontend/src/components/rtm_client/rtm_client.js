860d82c30c2fae3301971a7093cda3b6b33656be
Rashi Abramson <roshkins@gmail.com>
2017-09-09 14:41:03 -0700

    added rtm_client code

--- frontend/src/components/rtm_client/rtm_client.js
+++ frontend/src/components/rtm_client/rtm_client.js
@@ -1,6 +1,31 @@
 import RTM from "satori-rtm-sdk";
-class RTMClient {
-  constructor() {}
+class RTMChannel {
+  constructor(channelName, dataCallback, errorCallback) {
+    const appkey = "ACefAD440eE8EdeF52cf9eeACA0d5A1e";
+    const endpoint = "wss://ywf1mnw0.api.satori.com";
+    this.client = new RTM(endpoint, appkey);
+
+    this.client.on("error", function(error) {
+      var reason;
+      if (error.body) {
+        reason = error.body.error + " - " + error.body.reason;
+      } else {
+        reason = "unknown reason";
+      }
+      console.log("RTM client failed: " + reason);
+      errorCallback(error.body || "unknown error");
+    });
+
+    const channel = this.client.subscribe(
+      channelName,
+      RTM.SubscriptionMode.SIMPLE
+    );
+
+    /* set callback for PDU with specific action */
+    channel.on("rtm/subscription/data", function(pdu) {
+      dataCallback(pdu.body);
+    });
+  }
 }
 
-export default RTMClient;
+export default RTMChannel;
