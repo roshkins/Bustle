d3030ae12fce842d55d27e23c7ff2f5f38c813cf
Samtfm <sfmemail@gmail.com>
2017-09-09 15:39:13 -0700

    react google maps

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import './map.css';
 
+const googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyCvd5Ry4IK_o-C4LSNwdSuC1-gEhRSahlE"
+
+const GettingStartedGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={3}
+    defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+    onClick={props.onMapClick}
+  >
+    {props.markers.map((marker, index) => (
+      <Marker
+        {...marker}
+        onRightClick={() => props.onMarkerRightClick(index)}
+      />
+    ))}
+  </GoogleMap>
+));
+
 class Map extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div className="map">
-        <h2>MAP</h2>
-      </div>
+  render(){
+    return(
+      <div className='map'>
+        <GettingStartedGoogleMap
+          containerElement={
+            <div style={{ height: `100%` }} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+          onMapLoad={ () => console.log("mapload") }
+          onMapClick={ () => console.log("mapClick") }
+          markers={[]}
+          onMarkerRightClick={ () => console.log("mapRightClick") }
+        />
+    </div>
     );
   }
 }
