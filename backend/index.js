32f9c793702159f29aa702943125c4953b6f73d3
Rashi Abramson <roshkins@gmail.com>
2017-09-09 12:02:33 -0700

    can connect to RTM

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,29 @@ var RTM = require("satori-rtm-sdk");
 //   });
 
 // create an RTM client instance
-var rtm = new RTM(
+var client = new RTM(
   "wss://ywf1mnw0.api.satori.com",
   "ACefAD440eE8EdeF52cf9eeACA0d5A1e"
 );
+
+function showText(text) {
+  console.info(text);
+}
+// Hook up to client connectivity state transitions
+client.on("enter-connected", function() {
+  showText("Connected to Satori RTM!");
+});
+client.on("leave-connected", function() {
+  showText("Disconnected from Satori RTM");
+});
+client.on("error", function(error) {
+  var reason;
+  if (error.body) {
+    reason = error.body.error + " - " + error.body.reason;
+  } else {
+    reason = "unknown reason";
+  }
+  showText("RTM client failed: " + reason);
+});
+
+client.start();
