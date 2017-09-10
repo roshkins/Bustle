f3255ba5735b224ef40621c965bd0bcac27cc2bb
Samtfm <sfmemail@gmail.com>
2017-09-09 17:57:29 -0700

    show location marker

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -6,24 +6,35 @@ const googleMapURL =
   "https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyCvd5Ry4IK_o-C4LSNwdSuC1-gEhRSahlE&libraries=places";
 
 class Map extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {markers: []};
+  }
+
+
   mapsMount(googleMap) {
-    console.log("this.googleMap", googleMap.getDiv());
-    console.log("this.container", this.container);
-    this.service = new this.props.google.maps.places.PlacesService(
-      googleMap.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
-    );
-    //   this.container
+    // console.log("this.googleMap", googleMap.getDiv());
+    // console.log("this.container", this.container);
+    if(!this.hasMounted){
+      this.hasMounted = true;
+      this.service = new this.props.google.maps.places.PlacesService(
+        googleMap.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+      );
+    }
   }
 
   render() {
+    console.log(this.state.markers)
     const GettingStartedGoogleMap = withGoogleMap(props =>
       <GoogleMap
         ref={googleMap => {
           console.log("googleMapRef", googleMap);
+          this.googleMap = googleMap;
           this.mapsMount(googleMap);
         }}
-        defaultZoom={3}
-        defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+        defaultZoom={14}
+        defaultCenter={{ lat: 37.773568, lng: -122.4159416 }}
         onClick={props.onMapClick}
       >
         {props.markers.map((marker, index) =>
@@ -34,7 +45,6 @@ class Map extends Component {
         )}
       </GoogleMap>
     );
-    console.log("gm", this.googleMap);
 
     return (
       <div className="map">
@@ -48,14 +58,25 @@ class Map extends Component {
           }
           onMapLoad={() => console.log("mapload")}
           onMapClick={() => {
-            console.log(this.props.google);
             const request = {
-              query: "23 Olive lane",
+              location: this.googleMap.getCenter(),
+              query: "food",
               radius: "500"
             };
-            this.service.nearbySearch(request, r => console.log(r));
+            this.service.nearbySearch(request, r => {
+              const place = r[0];
+              const marker = {
+                position: {
+                  lat: place.geometry.location.lat(),
+                  lng: place.geometry.location.lng()
+                }
+              };
+              this.setState({markers: [...this.state.markers, marker]})
+              // this.setState();
+              // console.log(this.state.markers)
+            });
           }}
-          markers={[]}
+          markers={this.state.markers}
         />
       </div>
     );
