836357bc0db1ea972f35ee116c6719334ad6366d
Rashi Abramson <roshkins@gmail.com>
2017-09-10 09:08:34 -0700

    added spot locations

--- backend/index.js
+++ backend/index.js
@@ -80,6 +80,56 @@ channel.on("rtm/subscription/error", function(pdu) {
 });
 
 client.start();
+const spots = [
+  {
+    name: "Seminary",
+    id: 1,
+    direction: "North",
+    position: { lat: 37.8885981, lng: -122.5163723 }
+  },
+  {
+    name: "Tiburon Wye",
+    id: 2,
+    direction: "North",
+    position: { lat: 37.901499, lng: -122.515521 }
+  },
+  {
+    name: "Paradise Drive",
+    id: 3,
+    direction: "North",
+    position: {
+      lat: 37.9254,
+      lng: -122.513954
+    }
+  },
+  {
+    name: "Lucky Drive",
+    id: 4,
+    direction: "North",
+    position: {
+      lat: 37.938652,
+      lng: -122.516165
+    }
+  },
+  {
+    name: "Francisco Blvd",
+    id: 5,
+    direction: "North",
+    position: {
+      lat: 37.966483,
+      lng: -122.516137
+    }
+  },
+  {
+    name: "North San Pedro Rd",
+    id: 6,
+    direction: "North",
+    position: {
+      lat: 37.995662,
+      lng: -122.53253
+    }
+  }
+];
 
 setInterval(() => {
   const channelName = "riders";
